eeae1c358f8640e2c14d2938f677d4b927c8155d
gabrieldk <ganriel201@gmail.com>
2021-10-28 16:03:16 -0300

    funções de criação de usuário

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,6 @@
 import { inject, injectable } from "tsyringe";
+import { hash } from "bcrypt";
+
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -13,14 +15,20 @@ class CreateUserUseCase {
     name,
     driver_license,
     password,
-    username,
     email,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User Already Exists!!!");
+    }
+
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.creat({
       name,
       driver_license,
-      password,
-      username,
+      password: passwordHash,
       email,
     });
   }
