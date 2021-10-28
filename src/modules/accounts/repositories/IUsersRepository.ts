612f2c557ad73453eab911ab9d71488bb2803d56
gabrieldk <ganriel201@gmail.com>
2021-10-28 16:02:16 -0300

    repositório de usuário

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -1,7 +1,9 @@
 import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
+import { User } from "../entities/User";
 
 interface IUsersRepository {
   creat(data: ICreateUserDTO): Promise<void>;
+  findByEmail(email: string): Promise<User>;
 }
 
 export { IUsersRepository };
